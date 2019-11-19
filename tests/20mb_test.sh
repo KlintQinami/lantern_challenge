b05d6e649417478b7de8ed92e0d4937b159b3be6
klintqinami <kqinami@princeton.edu>
2019-11-19 10:04:17 -0500

    change test chunk size to make it faster

--- tests/20mb_test.sh
+++ tests/20mb_test.sh
@@ -4,7 +4,7 @@ test_url=http://212.183.159.230/20MB.zip
 opath_1=./test_file
 opath_2=./test_file_curl
 
-python ms_downloader.py --url $test_url --outfile-path $opath_1
+python ms_downloader.py --url $test_url --outfile-path $opath_1 --chunk-size 1000000
 curl -o $opath_2 $test_url > /dev/null 2>&1
 
 cmp $opath_1 $opath_2
